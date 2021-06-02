3a0e54eb49370d0aefdfb72e716cfd10c03c24f0
Guilherme Philippi <guilherme.philippi@hotmail.com>
2021-06-02 18:33:31 -0300

    initial commit

--- HCProtCLI/Program.cs
+++ HCProtCLI/Program.cs
@@ -7,6 +7,10 @@ namespace HCProtCLI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
