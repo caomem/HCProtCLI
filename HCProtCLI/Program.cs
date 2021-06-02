266098fbd7c958fba5780227729ab3a3e4d9d35e
Guilherme Philippi <guilherme.philippi@hotmail.com>
2021-06-02 18:13:10 -0300

    Add project files.

---
+++ HCProtCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HCProtCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
